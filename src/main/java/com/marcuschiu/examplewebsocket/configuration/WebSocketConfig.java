14b150cdb956aef02f68d15385b952d0ac23787e
Marcus Chiu <marcuschiu9@gmail.com>
2019-12-05 15:28:12 -0600

    comments and modified .gitignore file

--- src/main/java/com/marcuschiu/examplewebsocket/configuration/WebSocketConfig.java
+++ src/main/java/com/marcuschiu/examplewebsocket/configuration/WebSocketConfig.java
@@ -9,6 +9,12 @@ import org.springframework.web.socket.server.RequestUpgradeStrategy;
 import org.springframework.web.socket.server.standard.TomcatRequestUpgradeStrategy;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
+/**
+ * With @EnableWebSocketMessageBroker we enabled a broker-backed messaging over
+ * WebSocket using STOMP, which stands for Streaming Text Oriented Messaging Protocol.
+ * It's important to remark that this annotation needs to be used in
+ * conjunction with the @Configuration
+ */
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
@@ -28,6 +34,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.addEndpoint("/gs-guide-websocket")
                 .withSockJS();
 
+        // There's a reason why it has been separated (see link above)
         registry.addEndpoint("/gs-guide-websocket")
                 .setHandshakeHandler(new DefaultHandshakeHandler(upgradeStrategy))
                 .setAllowedOrigins("*");
